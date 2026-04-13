c48d3f0c2cb25bb4cc698cbb87bf0b1b171665fa
Gavin Smith <no reply>
2026-04-13 10:24:18 +0100

    Add Position Class plus Plateau Class

---
+++ MarsRover/Position.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mars_rover
+{
+    public class Position
+    {
+        public CompassDirection DirectionFacing { get; set; }
+        public int XCorodinate { get; set; }
+        public int YCorodinate { get; set;}
+    }
+}
