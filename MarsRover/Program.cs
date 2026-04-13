ed379ca58a5f5b8956f49f45d8751f26f074a40b
Gavin Smith <no reply>
2026-04-13 09:50:53 +0100

    Initial commit

---
+++ MarsRover/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace mars_rover
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
